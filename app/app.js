f40ab5bca60d92112b9a1da565c20c49f70889a0
yolixtly <yolixtly_13@hotmail.com>
2016-06-20 11:34:00 -0400

    factories

--- app/app.js
+++ app/app.js
@@ -5,83 +5,132 @@ app.config(['$routeProvider', function($routeProvider){
 		controller : 'HomeCtrl'
 	}).when('/newMeal', {
 		templateUrl : 'templates/new-meal.html',
-		controller: 'NewMealCtrl',
-		controllerAs : 'num'
+		controller: 'NewMealCtrl'
 	}).when('/earnings', {
 		templateUrl: 'templates/my-earnings.html',
-		controller: 'EarningsCtrl', 
-		controllerAs : 'num'
-
+		controller: 'EarningsCtrl'
 	});
 }]);
 
 
 
 app.controller('HomeCtrl',['$scope', '$rootScope', function($scope, $rootScope){
-//properties on the $scope
-$scope.$parent
+
 
 }]);
 
-app.controller('NewMealCtrl',['$scope', '$rootScope', function($scope, $rootScope){
+app.controller('NewMealCtrl',['$scope', '$rootScope', 'messageFactory', function($scope, $rootScope, messageFactory){
 
-		//Trigered by Cancel button
-		$scope.cancelValues = function(){
-		$scope.price = "";
-		$scope.tax = "";
-		$scope.tip ="";
-	 	};  
-
-		$scope.initCharge = function (){
+	$scope.check = messageFactory.check();  
+	$scope.initCharge = function (){
 		$scope.subtotalCharge = 0;
 		$scope.tipCharge = 0;
 		$scope.totalCharge = 0;	
 	};
 
-		//Submit Events 
+	$scope.initEarnings = function(){
+		$scope.tipTotal = 0;
+		$scope.mealCount = 0;
+		$scope.tipAverage = 0;
+	};
+
+	//Trigered by Cancel button
+	$scope.cancelValues = function(){
+	$scope.price = "";
+	$scope.tax = "";
+	$scope.tip ="";
+ 	};
+
+ 	//Trigered by Reset Button
+ 	$scope.resetAll = function() {
+ 		$scope.errorMsg= "";
+ 		$scope.cancelValues();
+ 		$scope.initCharge();
+ 		$scope.initEarnings();
+ 	};
+
+ 	//initial Values of App
+ 	$scope.resetAll();
+
+ 	//Submit Events 
 	$scope.submitForm = function(){
-		if($scope.num.myForm.$invalid){
+		if($scope.myForm.$invalid){
 			$scope.errorMsg = "Please enter valid numeric values";
 		} else {
+			$scope.check = messageFactory.calculateCharge($scope.tax, $scope.price, $scope.tip);
 			$scope.errorMsg = "";
 			$scope.subtotalCharge = $scope.price * (1 + ($scope.tax)/100);
 			$scope.tipCharge = $scope.subtotalCharge * ($scope.tip / 100); 
 	 		$scope.totalCharge = $scope.subtotalCharge + $scope.tipCharge;
 			$scope.mealCount++;
-			$scope.$parent.finalTips();
-			$scope.$parent.averageTip();
+			$scope.finalTips();
+			$scope.averageTip();
 			//Only resets price, tip and tax values
-			$scope.$parent.cancelValues();
-			$scope.$parent.initCharge();
+			$scope.cancelValues();
+
 		}	
 	};
+
+	$scope.finalTips = function(){
+		$scope.tipTotal+= $scope.tipCharge;
+	};
+
+	$scope.averageTip = function(){
+		$scope.tipAverage = $scope.tipTotal / $scope.mealCount;
+	};
+
+
 }]);
 
-app.controller('EarningsCtrl',['$scope', '$rootScope', function($scope, $rootScope){
-	
-	//Trigered by Reset Button
-	$rootScope.initEarnings = function(){
-		$rootScope.tipTotal = 0;
-		$rootScope.mealCount = 0;
-		$rootScope.tipAverage = 0;
-	};
+app.factory('messageFactory', function() {
+    var message = "Factory Hello World!";
+    subtotalCharge = 0;
+	tipCharge = 0; 
+	totalCharge = 0;
+	mealCount = 0;
+	tipTotal = 0;
+	tipAverage = 0;
 
-	$rootScope.finalTips = function(){
-		$rootScope.tipTotal+= $scope.tipCharge;
-	};
+    return {
+        check: function() {
+            return {
+            	subtotalCharge: subtotalCharge,
+            	tipCharge : tipCharge,
+            	totalCharge : totalCharge,
+            	mealCount : mealCount,
+            	tipTotal : tipTotal,
+            	tipAverage : tipAverage
+            };
 
-	$rootScope.averageTip = function(){
-		$rootScope.tipAverage = $rootScope.tipTotal / $rootScope.mealCount;
-	};
+        },
+        checkLastCharge: function() {
+            return {
+            	subtotalCharge: subtotalCharge,
+            	tipCharge : tipCharge,
+            	totalCharge : totalCharge
+            };
+        }, 
+        calculateCharge : function(tax, price, tip) {
+        	subtotalCharge = price * (1 + (tax)/100);
+			tipCharge = subtotalCharge * (tip / 100); 
+			totalCharge = subtotalCharge + tipCharge;
+			mealCount++;
+			tipTotal+= tipCharge;
+			tipAverage = tipTotal / mealCount;
+
+		   return {
+            	subtotalCharge: subtotalCharge,
+            	tipCharge : tipCharge,
+            	totalCharge : totalCharge,
+            	mealCount : mealCount,
+            	tipTotal : tipTotal,
+            	tipAverage : tipAverage
+            };	
+        }
+    };
+});
+
+app.controller('EarningsCtrl',['$scope', '$rootScope', 'messageFactory', function($scope, $rootScope, messageFactory){
+	$scope.check = messageFactory.check();  
 }]);
 
-// app.controller("CalculatorCtrl", function(){
-//  	$scope.resetAll = function() {
-//  		$scope.errorMsg= "";
-//  		$scope.cancelValues();
-//  		$scope.initEarnings();
-//  	};
-
-//  	//initial Values of App
-//  	resetAll();
-// });
