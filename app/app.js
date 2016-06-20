47be4c050bb9042ea8438d985d1128901b7d13a9
yolixtly <yolixtly_13@hotmail.com>
2016-06-20 12:36:20 -0400

    information not rendered

--- app/app.js
+++ app/app.js
@@ -2,26 +2,17 @@ var app = angular.module("myCalculator", ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/', {
 		templateUrl : 'templates/home.html',
-		controller : 'HomeCtrl'
+		controller : 'CalculatorCtrl'
 	}).when('/newMeal', {
 		templateUrl : 'templates/new-meal.html',
-		controller: 'NewMealCtrl'
+		controller: 'CalculatorCtrl'
 	}).when('/earnings', {
 		templateUrl: 'templates/my-earnings.html',
-		controller: 'EarningsCtrl'
+		controller: 'CalculatorCtrl'
 	});
 }]);
+app.controller('CalculatorCtrl', ['$scope', function($scope){
 
-
-
-app.controller('HomeCtrl',['$scope', '$rootScope', function($scope, $rootScope){
-
-
-}]);
-
-app.controller('NewMealCtrl',['$scope', '$rootScope', 'messageFactory', function($scope, $rootScope, messageFactory){
-
-	$scope.check = messageFactory.check();  
 	$scope.initCharge = function (){
 		$scope.subtotalCharge = 0;
 		$scope.tipCharge = 0;
@@ -57,7 +48,6 @@ app.controller('NewMealCtrl',['$scope', '$rootScope', 'messageFactory', function
 		if($scope.myForm.$invalid){
 			$scope.errorMsg = "Please enter valid numeric values";
 		} else {
-			$scope.check = messageFactory.calculateCharge($scope.tax, $scope.price, $scope.tip);
 			$scope.errorMsg = "";
 			$scope.subtotalCharge = $scope.price * (1 + ($scope.tax)/100);
 			$scope.tipCharge = $scope.subtotalCharge * ($scope.tip / 100); 
@@ -67,7 +57,6 @@ app.controller('NewMealCtrl',['$scope', '$rootScope', 'messageFactory', function
 			$scope.averageTip();
 			//Only resets price, tip and tax values
 			$scope.cancelValues();
-
 		}	
 	};
 
@@ -78,59 +67,4 @@ app.controller('NewMealCtrl',['$scope', '$rootScope', 'messageFactory', function
 	$scope.averageTip = function(){
 		$scope.tipAverage = $scope.tipTotal / $scope.mealCount;
 	};
-
-
 }]);
-
-app.factory('messageFactory', function() {
-    var message = "Factory Hello World!";
-    subtotalCharge = 0;
-	tipCharge = 0; 
-	totalCharge = 0;
-	mealCount = 0;
-	tipTotal = 0;
-	tipAverage = 0;
-
-    return {
-        check: function() {
-            return {
-            	subtotalCharge: subtotalCharge,
-            	tipCharge : tipCharge,
-            	totalCharge : totalCharge,
-            	mealCount : mealCount,
-            	tipTotal : tipTotal,
-            	tipAverage : tipAverage
-            };
-
-        },
-        checkLastCharge: function() {
-            return {
-            	subtotalCharge: subtotalCharge,
-            	tipCharge : tipCharge,
-            	totalCharge : totalCharge
-            };
-        }, 
-        calculateCharge : function(tax, price, tip) {
-        	subtotalCharge = price * (1 + (tax)/100);
-			tipCharge = subtotalCharge * (tip / 100); 
-			totalCharge = subtotalCharge + tipCharge;
-			mealCount++;
-			tipTotal+= tipCharge;
-			tipAverage = tipTotal / mealCount;
-
-		   return {
-            	subtotalCharge: subtotalCharge,
-            	tipCharge : tipCharge,
-            	totalCharge : totalCharge,
-            	mealCount : mealCount,
-            	tipTotal : tipTotal,
-            	tipAverage : tipAverage
-            };	
-        }
-    };
-});
-
-app.controller('EarningsCtrl',['$scope', '$rootScope', 'messageFactory', function($scope, $rootScope, messageFactory){
-	$scope.check = messageFactory.check();  
-}]);
-
